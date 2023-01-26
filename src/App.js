7b2c706ada0788fd2ad972a1d44f5cb2330edf9c
Mavdavis <ekwesimarvelous@gmail.com>
2023-01-26 15:35:21 +0100

    folder setup completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,96 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import {FiSettings} from 'react-icons/fi'
-const App = () => {
-  return <div>
-    <BrowserRouter>
-    
-    </BrowserRouter>
 
-  </div>;
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./Components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./Pages";
+import { FiSettings } from "react-icons/fi";
+const activeMenu = true;
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl hover:drop-shadow-xl hover:bg-light-gray p-3 text-white rounded-full"
+                style={{ background: "blue" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">sidebar</div>
+          )}
+          <div
+            className={`dark:bg-main-bg min-h-screen bg-main-bg w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {/* {themeSettings && (<ThemeSettings />)} */}
+
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Pages/Kanbaan.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Kanbaan = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Kanbaan
