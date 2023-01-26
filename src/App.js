fa100f1f047a8dac9bc4b8fd0f34faa594385829
Mavdavis <ekwesimarvelous@gmail.com>
2023-01-26 11:56:53 +0100

    Added data and folder setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import {FiSettings} from 'react-icons/fi'
 const App = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+  return <div>
+    <BrowserRouter>
+    
+    </BrowserRouter>
 
-export default App
+  </div>;
+};
+
+export default App;
