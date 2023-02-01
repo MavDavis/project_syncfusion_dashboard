07dee87dd91f473992f25ce8af9e2af6ca8fc4ea
Mavdavis <ekwesimarvelous@gmail.com>
2023-02-01 18:09:56 +0100

    employee modified

--- src/Pages/Employee.jsx
+++ src/Pages/Employee.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {GridComponent, ColumnsDirective, ColumnDirective,  Page,Search, Inject} from '@syncfusion/ej2-react-grids'
+import {GridComponent, ColumnsDirective, ColumnDirective,  Page,Search, Inject, Toolbar} from '@syncfusion/ej2-react-grids'
 import {employeesData, ContextMenuItems, employeesGrid} from '../data/dummy'
  import {Header} from '../Components'
 const Employee = () => {
  return (
    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
      <Header category='Page' title= 'Employees'/>
-     <GridComponent id='gridcomp' dataSource={employeesData} allowPaging allowSorting>
+     <GridComponent dataSource={employeesData} allowPaging allowSorting toolbar={['Search']} width='auto'>
        <ColumnsDirective>
        {employeesGrid.map((item,index)=>{
          return <ColumnDirective key={index} {...item}/>
        })}
        </ColumnsDirective>
-       <Inject services={ [Page,Search]}/>
+       <Inject services={ [Page,Search, Toolbar]}/>
      </GridComponent>
    </div>
  )
