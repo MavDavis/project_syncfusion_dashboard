95b466fd1eb7d0ed633257e7c12883d22f49a215
Mavdavis <ekwesimarvelous@gmail.com>
2023-02-02 17:21:02 +0100

    calendar page coming on

--- src/Pages/Customers.jsx
+++ src/Pages/Customers.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import {  customersData,customersGrid} 
+from '../data/dummy'
+import {GridComponent, ColumnsDirective, ColumnDirective,  Page,Selection, Inject, Toolbar, Filter, Sort, Edit} from '@syncfusion/ej2-react-grids'
+import { Header } from '../Components'
 const Customers = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <Header category='Page' title= 'Customers'/>
+    <GridComponent dataSource={customersData
+    } allowPaging allowSorting toolbar={['Search']} width='auto'>
+      <ColumnsDirective>
+      {customersGrid.map((item,index)=>{
+        return <ColumnDirective key={index} {...item}/>
+      })}
+      </ColumnsDirective>
+      <Inject services={ [Page, Toolbar, Selection, Edit, Sort, Filter]}/>
+    </GridComponent>
+  </div>
   )
 }
 
