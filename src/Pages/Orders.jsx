506458c6f52bbd00d7ff89d710a24c680b240700
Mavdavis <ekwesimarvelous@gmail.com>
2023-02-04 10:21:34 +0100

    commit

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -6,6 +6,7 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title= 'Orders'/>
+      {/* <div className="items-center"></div> */}
       <GridComponent id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
         {ordersGrid.map((item,index)=>{
